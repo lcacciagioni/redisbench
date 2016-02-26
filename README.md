f67cfee701dd5c16006c370e83b893013d93c18f
Leandro Cacciagioni <leandro.21.2008@gmail.com>
2016-02-26 16:02:28 +0100

    Added cluster

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ A POC of a stress tester for redis in golang
 
 ## TODO
 
-* test
-* cluster aware
-* bynary data maybe using `[]byte('str')` in place of a string
-* more information to be displayed
-* optimizations to randomization of strings
+* [ ] test
+* [x]cluster aware
+  * I have tested only locally with docker-compose so if any of you can test it a little more is welcome.
+* [ ] bynary data maybe using `[]byte('str')` in place of a string
+* [ ] more information to be displayed
+* [x]optimizations to randomization of strings
+  * God save [stackoverflow](https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang/)
+
+> All PR are welcome is nice to know that this can be useful for someone else.
--- redisbench.go
+++ redisbench.go
@@ -1,6 +1,5 @@
 package main
 
-import "fmt"
 import "flag"
 import "strconv"
 import "math/rand"
@@ -8,63 +7,130 @@ import "math/rand"
 import "time"
 import "log"
 
-import "github.com/garyburd/redigo/redis"
+import "strings"
+import redigo "github.com/garyburd/redigo/redis"
+import redigocluster "github.com/chasex/redis-go-cluster"
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-// RandStringBytes takes the size of the message and generates a pseudo
-// random string
-func RandStringBytes(n int) string {
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+// RandStringBytesMaskImprSrc is a function highly optimized for generation of
+// random strings in golang
+func RandStringBytesMaskImprSrc(n int) string {
 	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
 	}
+
 	return string(b)
 }
 
 // RandRange returns a random string with using the min and max values as limit.
 func RandRange(min, max int) string {
 	randsize := rand.Intn(max - min)
-	return RandStringBytes(min + randsize)
+	return RandStringBytesMaskImprSrc(min + randsize)
+}
+
+// StressCluster is a simple function to stress test a redis cluster
+func StressCluster(hosts []string, minMsgSize, maxMsgSize, numOfMsg int) {
+	c, err := redigocluster.NewCluster(&redigocluster.Options{
+		StartNodes:   hosts,
+		ConnTimeout:  50 * time.Millisecond,
+		ReadTimeout:  50 * time.Millisecond,
+		WriteTimeout: 50 * time.Millisecond,
+		KeepAlive:    16,
+		AliveTime:    60 * time.Second,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Starting SET")
+	startSet := time.Now()
+	for i := 0; i <= numOfMsg; i++ {
+		rediskey := "message" + strconv.Itoa(i)
+		message := RandRange(minMsgSize, maxMsgSize)
+		//set
+		c.Do("SET", rediskey, message)
+	}
+	log.Println("SET of ", strconv.Itoa(numOfMsg), " random messages has taken:",
+		time.Since(startSet))
+
+	log.Println("Starting GET")
+	startGet := time.Now()
+	for j := 0; j <= numOfMsg; j++ {
+		rediskey := "message" + strconv.Itoa(j)
+		//get
+		_, err := redigo.String(c.Do("GET", rediskey))
+		if err != nil {
+			log.Println("key not found")
+		}
+	}
+	log.Println("GET of ", strconv.Itoa(numOfMsg), " random messages has taken:", time.Since(startGet))
+}
+
+// StressNode is fuction that executes a series of stress tests in a sigle node
+// of redis
+func StressNode(host string, minMsgSize, maxMsgSize, numOfMsg int) {
+	c, err := redigo.Dial("tcp", host)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer c.Close()
+	log.Println("Starting SET")
+	startSet := time.Now()
+	for i := 0; i <= numOfMsg; i++ {
+		rediskey := "message" + strconv.Itoa(i)
+		message := RandRange(minMsgSize, maxMsgSize)
+		//set
+		c.Do("SET", rediskey, message)
+	}
+	log.Println("SET of ", strconv.Itoa(numOfMsg), " random messages has taken:",
+		time.Since(startSet))
+
+	log.Println("Starting GET")
+	startGet := time.Now()
+	for j := 0; j <= numOfMsg; j++ {
+		rediskey := "message" + strconv.Itoa(j)
+		//get
+		_, err := redigo.String(c.Do("GET", rediskey))
+		if err != nil {
+			log.Println("key not found")
+		}
+	}
+	log.Println("GET of ", strconv.Itoa(numOfMsg), " random messages has taken:", time.Since(startGet))
 }
 
 func main() {
 	minMsgSizePtr := flag.Int("minMsgSize", 100, "The minimun size of the massages to send in bytes")
 	maxMsgSizePtr := flag.Int("maxMsgSize", 1000, "The maximun size of the massages to send in bytes")
 	numOfMsgPtr := flag.Int("numOfMsg", 10000, "The number of messages to send")
-	redisHostPtr := flag.String("redisHost", "localhost", "The host where redis is located.")
-	redisPortPtr := flag.Int("redisPort", 6379, "The port where redis is listening")
+	redisNodes := flag.String("redisNodes", "127.0.0.1:6379", "Cluster nodes declaration splitted by comma.")
 	flag.Parse()
 	if *minMsgSizePtr > *maxMsgSizePtr {
 		log.Fatalln("minMsgSize (", *minMsgSizePtr, ")can't be bigger than maxMsgSize (", *maxMsgSizePtr, ").")
 	}
-	//INIT OMIT
-	c, err := redis.Dial("tcp", *redisHostPtr+":"+strconv.Itoa(*redisPortPtr))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer c.Close()
 
-	fmt.Println("Starting SET")
-	startSet := time.Now()
-	for i := 0; i <= *numOfMsgPtr; i++ {
-		rediskey := "message" + strconv.Itoa(i)
-		message := RandRange(*minMsgSizePtr, *maxMsgSizePtr)
-		//set
-		c.Do("SET", rediskey, message)
+	if len(strings.Split(*redisNodes, ",")) == 1 {
+		StressNode(*redisNodes, *minMsgSizePtr, *maxMsgSizePtr, *numOfMsgPtr)
+	} else {
+		StressCluster(strings.Split(*redisNodes, ","), *minMsgSizePtr, *maxMsgSizePtr, *numOfMsgPtr)
 	}
-	fmt.Println("SET of ", strconv.Itoa(*numOfMsgPtr), " random messages has taken:", time.Since(startSet))
 
-	fmt.Println("Starting GET")
-	startGet := time.Now()
-	for j := 0; j <= *numOfMsgPtr; j++ {
-		rediskey := "message" + strconv.Itoa(j)
-		//get
-		_, err := redis.String(c.Do("GET", rediskey))
-		if err != nil {
-			fmt.Println("key not found")
-		}
-	}
-	fmt.Println("GET of ", strconv.Itoa(*numOfMsgPtr), " random messages has taken:", time.Since(startGet))
 	//ENDINIT OMIT
 }
