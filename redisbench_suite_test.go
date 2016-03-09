70c78ca6d932d4fce908bbaee031a01c1743609f
Leandro Cacciagioni <leandro.21.2008@gmail.com>
2016-03-09 16:55:57 +0100

    Fixed codeclimate suggest

--- redisbench_suite_test.go
+++ redisbench_suite_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+// TestRedisbench is the generic handler that ginkgo uses to test
 func TestRedisbench(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Redisbench Suite")
